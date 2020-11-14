058cb0b61e342dab91a475f898f7d643de823aa9
doudou09261216 <1617827093@qq.com>
2020-11-14 17:07:50 +0800

    feature:完成项目git管理

---
+++ src/app.js
@@ -0,0 +1,16 @@
+
+
+
+const Koa = require('koa')
+
+const app = new Koa()
+
+
+
+app.use(ctx => {
+    ctx.body = '微信小程序访问了我！';
+});
+
+app.listen(3000, () => {
+    console.log("Server is running at 3000")
+});
